01c494061af43a48de51dc07389642bea4bb6aa1
Jens Jovellano <jensjovellano@gmail.com>
2023-08-04 10:09:20 +0800

    deleted demo files and modified src files

--- src/exercise-1.js
+++ src/exercise-1.js
@@ -1,5 +1,12 @@
 const getCommonItems = (array1, array2) => {
   // implement code here
+  const common = []
+  array1.forEach((itemToFind) => {
+    array2.forEach((item) => {
+      if(item === itemToFind) common.push(item)
+    })
+  })
+  return common
 };
 
 export default getCommonItems;
--- src/exercise-2.js
+++ src/exercise-2.js
@@ -1,5 +1,10 @@
 const getTheExactNumber = (numbers) => {
   // implement code here
+  const multiples = []
+  numbers.forEach((num) => {
+    if (num % 3 === 0) multiples.push(num)
+  })
+  return Math.max(...multiples)
 }
 
 export default getTheExactNumber;
